65de87e5ce76d7016cd4752fc3f441d39a5e4191
lizhong-bi <39711443+lizhong-bi@users.noreply.github.com>
2018-12-19 15:53:04 -0500

    Uploaded the src folder of the project
    Due to the capacity that GitHub allows me to upload the files, I was only able to upload the src file without any issue.

---
+++ src/main/java/com/example/lizhongbi/fragment/landingPage.java
@@ -0,0 +1,77 @@
+package com.example.lizhongbi.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class landingPage extends AppCompatActivity implements buttonFragment.onClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+      /**  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        **/
+        buttonFragment firstFragment = new buttonFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.button, firstFragment,firstFragment.getTag()).commit();
+
+        canvasFragment secondFragment = new canvasFragment();
+        secondFragment.setVar(message);
+        manager.beginTransaction().replace(R.id.canvas, secondFragment,secondFragment.getTag()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View view)
+    {
+        onCanvasSelected();
+    }
+
+
+    @Override
+    public void onCanvasSelected() {
+        canvasFragment canvas = (canvasFragment) getSupportFragmentManager().findFragmentById(R.id.canvas);
+        canvas.changeData();
+    }
+}
